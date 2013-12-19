ed50e092d0320e97cab6c1e467c49e34845a28c0
Alfred Bez <alfred.bez@googlemail.com>
2013-12-19 10:21:10 +0100

    Updated Version (1.0)

--- js/script.js
+++ js/script.js
@@ -36,7 +36,7 @@ var num = 31,
     ];
 
 $(document).ready(function() {
-    function shuffle(o) { //v1.0
+    function shuffle(o) {
         for (var j, x, i = o.length; i; j = Math.floor(Math.random() * i), x = o[--i], o[i] = o[j], o[j] = x);
         return o;
     }
